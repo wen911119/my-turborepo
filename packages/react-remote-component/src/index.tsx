99ad311cd58f71e43fc9c1332a2a150b550d32b0
wen911119 <wen911119@gmail.com>
2024-06-27 02:24:32 +0800

    feat: add react-remote-component

---
+++ packages/react-remote-component/src/index.tsx
@@ -0,0 +1,33 @@
+import React, { type FC, Suspense, type ReactNode, lazy, useRef, Fragment } from "react";
+import {
+  RemoteCjsModuleLoader as loader,
+  type RemoteCjsModuleLoaderParam,
+} from "@microfe/remote-cjs-module-loader";
+
+export interface RemoteComponentProps extends RemoteCjsModuleLoaderParam {
+  loadingComponent?: ReactNode;
+  [key: string]: any;
+}
+
+export const DefaultLoadingComponent:FC = () => <Fragment>loading...</Fragment>;
+
+export const defaultDependencies = {
+  react: React,
+};
+
+export const RemoteComponent: FC<RemoteComponentProps> = ({
+  url,
+  dependencies = defaultDependencies,
+  fetcher,
+  loadingComponent = <DefaultLoadingComponent />,
+  ...otherProps
+}) => {
+  const LazyComponent = useRef(
+    lazy(() => loader({ url, dependencies, fetcher }))
+  );
+  return (
+    <Suspense fallback={loadingComponent}>
+      <LazyComponent.current {...otherProps} />
+    </Suspense>
+  );
+};
--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
