0f1edbe75842b77a491186b9b4d4f1522bc355d1
wen911119 <wen911119@gmail.com>
2024-06-28 02:16:53 +0800

    feat: delete useless file

--- apps/demos/stories/button.stories.tsx
+++ apps/demos/stories/button.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { Button } from "@acme/ui/button";
+import { Button } from "./button";
 
 const meta: Meta<typeof Button> = {
   component: Button,
